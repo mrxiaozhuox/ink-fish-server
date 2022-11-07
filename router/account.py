05b3fb9ec866d5805ce9821b1298e402b72a553c
YuKun Liu <mrxzx.info@gmail.com>
2022-11-07 10:08:35 -0800

    feat: commit code

--- router/account.py
+++ router/account.py
@@ -1,7 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+
+from database import account
+
+from sqlalchemy.orm import Session
+from database.connection import get_database
 
 router = APIRouter(prefix="/account")
 
-@router.get("/test")
-async def account_test_api():
-    return { "message": "Hello World" }
+
+@router.get("/info/", response_model=account.UserInfo)
+async def get_account_info(
+    id: str | None = None,
+    email: str | None = None, 
+    db: Session = Depends(get_database)
+    ):
+
+    if id is not None:
+        user = account.get_user(db, id)
+    elif email is not None:
+        user = account.get_user_by_email(db, email)
+    else:
+        user = None
+
+    if user is None:
+        raise HTTPException(status_code=400, detail="User not found")
+    return user
+
+@router.post("/register/")
+def register_account():
+    return {}
