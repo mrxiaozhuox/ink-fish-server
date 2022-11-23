771fe886af77d38c1641094fb32635c993550c54
YuKun Liu <mrxzx.info@gmail.com>
2022-11-22 23:09:07 -0800

    feat: commit code

--- src/router/user.rs
+++ src/router/user.rs
@@ -23,6 +23,25 @@ pub async fn get_account(req: &mut Request, _resp: &mut Response) -> Result<Json
     Ok(Json(user))
 }
 
+#[handler]
+pub async fn user_login(req: &mut Request) -> Result<Json<UserReader>> {
+
+    let email = req.query::<String>("email");
+    let password = req.query::<String>("password");
+
+    if email.is_none() || password.is_none() {
+        return Err(AppError::MissingParam("Email or Password"));
+    }
+    let email = email.unwrap();
+    let password = password.unwrap();
+
+    let user = User::get_user_by_email(email).await?;
+
+    // User::gen_hashed_password(user.);
+
+    todo!()
+}
+
 pub fn router() -> Router {
     Router::with_path("user").push(Router::with_path("info").get(get_account))
 }
